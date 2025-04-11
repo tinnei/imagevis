5a6f26a4071fefa7158073f52e0cddc3f24c498d
Tinnei Pang <tinneipang@gmail.com>
2025-04-11 15:59:31 -0400

    initial commit with sunflower

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <BasicScene msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import BasicScene from './components/basicScene.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BasicScene
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background: black;
+}
+</style>
